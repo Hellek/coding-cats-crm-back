6e37173e8546a1d793ed2a0cf6166f4a7e382340
Roman Basharin <r.basharin@itigris.ru>
2020-10-31 01:45:19 +0300

    Вывел валидацию авторизационных данных пользователя в метод

--- src/services/auth/model.js
+++ src/services/auth/model.js
@@ -4,13 +4,23 @@ import UsersModel from '../users/model'
 const Users = new UsersModel
 
 class Auth {
-	async authentication({ email, password }) {
+	async validateAuthData({ email, password }) {
 		if (!email) throw Error('Не указан email')
 		if (!password) throw Error('Не указан пароль')
 		if (!await isEmailValid(email)) throw Error('Не валидный емейл, проверьте написание')
+	}
+
+	async getValidUser({ email, password }) {
+		await this.validateAuthData({ email, password })
 		const user = await Users.getByEmail({ email, getPassword: true })
-		if (user == null || !await compareHash(password, user.password)) throw Error('Неверная пара логин-пароль')
+		console.log(user)
 		if (!user.active) throw Error('Доступ к системе запрещён')
+		return user
+	}
+
+	async authentication({ email, password }) {
+		const user = await this.getValidUser({ email, password })
+		if (user == null || !await compareHash(password, user.password)) throw Error('Неверная пара логин-пароль')
 
 		// Удаляем пароль и не нужные данные
 		delete user.password
