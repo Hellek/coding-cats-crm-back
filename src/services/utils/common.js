609b155aa58c8f83b6a2576ae68379a451451c18
Roman Basharin <r.basharin@itigris.ru>
2021-01-30 13:29:27 +0300

    Добавлен шифровальщик токенов

--- src/services/utils/common.js
+++ src/services/utils/common.js
@@ -1,5 +1,6 @@
 import isemail from 'isemail'
 import bcryptjs from 'bcryptjs'
+import Cryptr from 'cryptr'
 
 export async function compareHash(stringToCompare, hash) {
 	return bcryptjs.compare(stringToCompare, hash)
@@ -12,3 +13,5 @@ export async function generateHash(stringToHashing, saltRounds = 5) {
 export async function isEmailValid(email) {
 	return isemail.validate(email)
 }
+
+export const cryptrTokens = new Cryptr(JSON.parse(process.env.SECRET_KEYS).tokens)
