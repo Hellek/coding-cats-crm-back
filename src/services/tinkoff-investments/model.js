80a17b67f4e7bcfac81d88abed2bd4938bf1a72f
Roman Basharin <roman.basharin@quantori.com>
2021-06-05 11:54:21 +0300

    tinkoff-api-backup

--- src/services/tinkoff-investments/model.js
+++ src/services/tinkoff-investments/model.js
@@ -65,6 +65,47 @@ class TinkoffInvestments {
 
 		return await api.portfolio()
 	}
+
+	async portfolioCurrencies({ user }) {
+		// const api = this.constructTikkoffApi({ user })
+		// return (await api.accounts()).accounts
+	}
+
+	async search({ user }) {
+		// const api = this.constructTikkoffApi({ user })
+		// return (await api.accounts()).accounts
+		// ▸ search(params: InstrumentId): Promise‹MarketInstrumentList›
+		// Метод для поиска инструментов по figi или ticker
+	}
+
+	async test({ user }) {
+		/* const isRealMode = false
+		const apiURL = isRealMode ? 'https://api-invest.tinkoff.ru/openapi' : 'https://api-invest.tinkoff.ru/openapi/sandbox/'
+		const socketURL = 'wss://api-invest.tinkoff.ru/openapi/md/v1/md-openapi/ws'
+		const secretToken = isRealMode ? user.TIRealToken : user.TISandboxToken
+
+		const api = new OpenAPI({
+			apiURL,
+			secretToken,
+			socketURL,
+		})
+
+		await api.sandboxClear()
+
+		const marketInstrument = await api.searchOne({ ticker: 'AAPL' })
+
+		return marketInstrument
+
+		console.log(MarketInstrument) */
+
+		// const { figi } = marketInstrument
+		// console.log(await api.setCurrenciesBalance({ currency: 'USD', balance: 1000 })); // 1000$ на счет
+		// console.log(await api.portfolioCurrencies());
+		// console.log(await api.instrumentPortfolio({ figi })); // В портфеле ничего нет
+		// console.log(await api.limitOrder({ operation: 'Buy', figi, lots: 1, price: 100 })); // Покупаем AAPL
+		// console.log(await api.instrumentPortfolio({ figi })); // Сделка прошла моментально
+		// console.log(await api.orderbookGet({ figi })); // получаем стакан по AAPL
+	}
 }
 
 export default TinkoffInvestments
