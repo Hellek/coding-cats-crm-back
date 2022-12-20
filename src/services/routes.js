bf0424accaaf1f19117ded3d2ba7e6c9ae9d1bc7
Roman Basharin <roman.basharin@quantori.com>
2022-12-20 19:37:53 +0700

    Just external service stayed

--- src/services/routes.js
+++ src/services/routes.js
@@ -1,19 +1,19 @@
-import auth from './auth/http'
+// import auth from './auth/http'
 import external from './external/http'
-import ideas from './ideas/http'
-import instruments from './instruments/http'
-import users from './users/http'
-import roles from './roles/http'
-import tinkoffInvestments from './tinkoff-investments/http'
+// import ideas from './ideas/http'
+// import instruments from './instruments/http'
+// import users from './users/http'
+// import roles from './roles/http'
+// import tinkoffInvestments from './tinkoff-investments/http'
 
 function setServiceRoutes(router) {
-	router.use('/auth', auth)
+	// router.use('/auth', auth)
 	router.use('/external', external)
-	router.use('/ideas', ideas)
-	router.use('/instruments', instruments)
-	router.use('/users', users)
-	router.use('/roles', roles)
-	router.use('/tinkoff-investments', tinkoffInvestments)
+	// router.use('/ideas', ideas)
+	// router.use('/instruments', instruments)
+	// router.use('/users', users)
+	// router.use('/roles', roles)
+	// router.use('/tinkoff-investments', tinkoffInvestments)
 }
 
 export default setServiceRoutes
